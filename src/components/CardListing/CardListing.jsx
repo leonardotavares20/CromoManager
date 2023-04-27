973a83c308a1c2bede2f10fc3258906f4117893a
Leonardo Silva <leo.tav.12345@gmail.com>
2023-04-27 18:09:20 -0300

    Card Listing Adicionada
    Componente card listing adicionada

--- src/components/CardListing/CardListing.jsx
+++ src/components/CardListing/CardListing.jsx
@@ -1,20 +1,89 @@
 export default CardListing
-import './CardListing'
+import './CardListing.css'
 
 function CardListing() {
     return(
         <>
-            <section>
-                <div>
+            <section className='CardListing'>
+                <div className='Cabecalho'>
                     <form action="">
                         <label htmlFor="">Exibir</label>
                         <input type="text" name="" id="" />
                         <label htmlFor="">Itens</label>
                     </form>
                 </div>
-                <table>
-                    
-                </table>
+                <div className='TableOver'>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>Ticket</th>
+                                <th>Assunto</th>
+                                <th>Status</th>
+                                <th>Última interação</th>
+                            </tr>
+                            <tr>
+                                <td>368711</td>
+                                <td>Email</td>
+                                <td>Aguardando sua resposta</td>
+                                <td>22/01/2023 11:07</td>
+                            </tr>
+                            <tr>
+                                <td>368711</td>
+                                <td>Email</td>
+                                <td>Em tratamento</td>
+                                <td>22/01/2023 11:07</td>
+                            </tr>
+                            <tr>
+                                <td>368711</td>
+                                <td>Email</td>
+                                <td>Resolvido</td>
+                                <td>22/01/2023 11:07</td>
+                            </tr>
+                            <tr>
+                                <td>368711</td>
+                                <td>Email</td>
+                                <td>Resolvido</td>
+                                <td>22/01/2023 11:07</td>
+                            </tr>
+                            <tr>
+                                <td>368711</td>
+                                <td>Email</td>
+                                <td>Resolvido</td>
+                                <td>22/01/2023 11:07</td>
+                            </tr>
+                            <tr>
+                                <td>368711</td>
+                                <td>Email</td>
+                                <td>Resolvido</td>
+                                <td>22/01/2023 11:07</td>
+                            </tr>
+                            <tr>
+                                <td>368711</td>
+                                <td>Email</td>
+                                <td>Resolvido</td>
+                                <td>22/01/2023 11:07</td>
+                            </tr>
+                            <tr>
+                                <td>368711</td>
+                                <td>Email</td>
+                                <td>Resolvido</td>
+                                <td>22/01/2023 11:07</td>
+                            </tr>
+                            <tr>
+                                <td>368711</td>
+                                <td>Email</td>
+                                <td>Resolvido</td>
+                                <td>22/01/2023 11:07</td>
+                            </tr>
+                            <tr>
+                                <td>368711</td>
+                                <td>Email</td>
+                                <td>Resolvido</td>
+                                <td>22/01/2023 11:07</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </section>
         </>
     )
