89bfd8520b5d7e9d60befebae05764048307f7e9
Leonardo Silva <leo.tav.12345@gmail.com>
2023-05-02 13:01:52 -0300

    Styled components, fazendo a mudança

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BotaoNormal } from './components/Buttons/Buttons'
 import AdministrativePanel from './components/AdministrativePanel/AdministrativePanel'
 import Cards from "./components/Cards/Cards"
 import Forms from "./components/Forms/Forms"
@@ -8,11 +9,11 @@ import CardOpenCall from './components/CardOpenCall/CardOpenCall'
 function App() {
   return (
     <>
-    {/* <NavBar></NavBar> */}
-    {/* <AdministrativePanel></AdministrativePanel> */}
-    {/* <Cards></Cards>
-    <Forms></Forms> */}
-    {/* <CardListing></CardListing> */}
+    <NavBar></NavBar>
+    <AdministrativePanel></AdministrativePanel>
+    <Cards></Cards>
+    <Forms></Forms>
+    <CardListing></CardListing>
     <CardOpenCall></CardOpenCall>
     </>
   )
