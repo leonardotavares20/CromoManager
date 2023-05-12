da17043ff9c9f67d22ee06d498c432b7bdbbb415
Leonardo Silva <leo.tav.12345@gmail.com>
2023-05-11 23:01:22 -0300

    Adicionando funcao construtora no CardListBanner

--- src/components/CardListBanner/CardListBanner.jsx
+++ src/components/CardListBanner/CardListBanner.jsx
@@ -11,86 +11,25 @@ import { TableListBanner,
 } from './style/CardListBanner'
 
 function CardListBanner() {
+    function Banner(ID, Titulo, Categoria, Status) {
+        this.ID = ID;
+        this.Titulo = Titulo;
+        this.Categoria = Categoria;
+        this.Status = Status;
+        this.Ações = <SwitchButton></SwitchButton>;
+      }
+
     const dados = [  
-                    { 
-                        ID: 1,
-                        Titulo: 'Banner Topo Home', 
-                        Categoria: 'Topo - Home', 
-                        Status: 'Publicado', 
-                        Ações: <SwitchButton></SwitchButton> 
-                    },
-
-                    { 
-
-                        ID: 2, 
-                        Titulo: 'Banner Topo Home', 
-                        Categoria: 'Topo - Home', 
-                        Status: 'Publicado', 
-                        Ações: <SwitchButton></SwitchButton> 
-                    }, 
-
-                    {   
-                        ID: 3, 
-                        Titulo: 'Banner Topo Home', 
-                        Categoria: 'Topo - Home', 
-                        Status: 'Publicado', 
-                        Ações: <SwitchButton></SwitchButton> 
-                    },
-
-                    { 
-                        ID: 4, 
-                        Titulo: 'Banner Topo Home', 
-                        Categoria: 'Topo - Home', 
-                        Status: 'Publicado', 
-                        Ações: <SwitchButton></SwitchButton> 
-                    },
-
-                    { 
-                        ID: 5, 
-                        Titulo: 'Banner Topo Home', 
-                        Categoria: 'Topo - Home', 
-                        Status: 'Publicado', 
-                        Ações: <SwitchButton></SwitchButton> 
-                    },
-
-                    { 
-                        ID: 6, 
-                        Titulo: 'Banner Topo Home', 
-                        Categoria: 'Topo - Home', 
-                        Status: 'Publicado', Ações: <SwitchButton></SwitchButton> 
-                    },
-
-                    { 
-                        ID: 7, 
-                        Titulo: 'Banner Topo Home', 
-                        Categoria: 'Topo - Home', 
-                        Status: 'Publicado', 
-                        Ações: <SwitchButton></SwitchButton> 
-                    },
-
-                    { 
-                        ID: 8, 
-                        Titulo: 'Banner Topo Home', 
-                        Categoria: 'Topo - Home', 
-                        Status: 'Publicado', 
-                        Ações: <SwitchButton></SwitchButton> 
-                    },
-
-                    { 
-                        ID: 9, 
-                        Titulo: 'Banner Topo Home', 
-                        Categoria: 'Topo - Home', 
-                        Status: 'Publicado', 
-                        Ações: <SwitchButton></SwitchButton> 
-                    },
-
-                    { 
-                        ID: 10, 
-                        Titulo: 'Banner Topo Home', 
-                        Categoria: 'Topo - Home', 
-                        Status: 'Publicado', 
-                        Ações: <SwitchButton></SwitchButton> 
-                    },
+                    new Banner(1, 'Banner Topo Home', 'Topo-Home', 'Publicado'),
+                    new Banner(2, 'Banner Topo Home', 'Topo-Home', 'Publicado'),
+                    new Banner(3, 'Banner Topo Home', 'Topo-Home', 'Publicado'),
+                    new Banner(4, 'Banner Topo Home', 'Topo-Home', 'Publicado'),
+                    new Banner(5, 'Banner Topo Home', 'Topo-Home', 'Publicado'),
+                    new Banner(6, 'Banner Topo Home', 'Topo-Home', 'Publicado'),
+                    new Banner(7, 'Banner Topo Home', 'Topo-Home', 'Publicado'),
+                    new Banner(8, 'Banner Topo Home', 'Topo-Home', 'Publicado'),
+                    new Banner(9, 'Banner Topo Home', 'Topo-Home', 'Publicado'),
+                    new Banner(10, 'Banner Topo Home', 'Topo-Home', 'Publicado')
                 ];
     return(
         <>
